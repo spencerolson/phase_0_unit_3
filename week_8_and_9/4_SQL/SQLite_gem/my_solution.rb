9b3066ca3e27c60b13e5ddcccef02e6967a4993b
Spencer Olson <spence1818@gmail.com>
2014-05-25 16:37:47 -0500

    SQL Congress Challenge Complete

--- week_8_and_9/4_SQL/SQLite_gem/my_solution.rb
+++ week_8_and_9/4_SQL/SQLite_gem/my_solution.rb
@@ -1,6 +1,4 @@
-# U3.W7: BONUS Using the SQLite Gem
-
-# I worked on this challenge [by myself, with:]
+# I worked on this challenge with Celeen Rusk
 
 require 'sqlite3'
 
@@ -8,19 +6,24 @@ require 'sqlite3'
 $db = SQLite3::Database.open "congress_poll_results.db"
 
 
-def print_arizona_reps
-  puts "AZ REPRESENTATIVES"
-  az_reps = $db.execute("SELECT name FROM congress_members WHERE location = 'AZ'")
-  az_reps.each { |rep| puts rep }
+def print_state_reps(*states)
+	states.each do |state| state.upcase!
+  puts "#{state} REPRESENTATIVES"
+  state_reps = $db.execute("SELECT name FROM congress_members WHERE location = '#{state}'")
+  state_reps.each { |rep| puts rep }
+  print_separator
+	end
 end
 
 def print_longest_serving_reps(minimum_years)  #sorry guys, oracle needs me, i didn't finish this!
   puts "LONGEST SERVING REPRESENTATIVES"
-  puts $db.execute("SELECT name FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  reps_with_years = $db.execute("SELECT name, years_in_congress FROM congress_members WHERE years_in_congress > #{minimum_years}") 
+  reps_with_years.each{|rep_with_year_arr| puts "#{rep_with_year_arr[0]} - #{rep_with_year_arr[1]} years"}
 end
 
-def print_lowest_grade_level_speakers
-  puts "LOWEST GRADE LEVEL SPEAKERS (less than < 8th grade)"
+def print_lowest_grade_level_speakers(grade_current)
+  puts "LOWEST GRADE LEVEL SPEAKERS (less than #{grade_current}th grade)"
+  puts $db.execute("SELECT name FROM congress_members WHERE grade_current < #{grade_current}")
 end
 
 def print_separator
@@ -29,8 +32,15 @@ def print_separator
   puts 
 end
 
+def print_all_politicians_with_votes
+	puts "ALL POLITICIANS WITH VOTES"
+	all_politicians = $db.execute("SELECT name, COUNT(politician_id) FROM congress_members JOIN votes ON (congress_members.id = politician_id) GROUP BY congress_members.id")
+	all_politicians.each{|politician| puts "#{politician[0]} - #{politician[1]} votes"}
+end
 
-print_arizona_reps
+
+
+print_state_reps("az")
 
 print_separator
 
@@ -39,12 +49,13 @@ print_longest_serving_reps(35)
 # output should look like:  Rep. C. W. Bill Young - 41 years
 
 print_separator
-print_lowest_grade_level_speakers 
+print_lowest_grade_level_speakers(8)
 # TODO - Need to be able to pass the grade level as an argument, look in schema for "grade_current" column
 
 # TODO - Make a method to print the following states representatives as well:
 # (New Jersey, New York, Maine, Florida, and Alaska)
-
+print_separator
+print_state_reps("nJ", "ny", "me", "fl", "ak")
 
 ##### BONUS #######
 # TODO (bonus) - Stop SQL injection attacks!  Statmaster learned that interpolation of variables in SQL statements leaves some security vulnerabilities.  Use the google to figure out how to protect from this type of attack.
@@ -53,6 +64,10 @@ print_lowest_grade_level_speakers
 # Create a listing of all of the Politicians and the number of votes they recieved
 # output should look like:  Sen. John McCain - 7,323 votes (This is an example, yours will not return this value, it should just 
 #    have a similar format)
+print_separator
+print_all_politicians_with_votes
+
+
 # Create a listing of each Politician and the voter that voted for them
 # output should include the senators name, then a long list of voters separated by a comma
 #
@@ -67,3 +82,5 @@ print_lowest_grade_level_speakers
 #   > #{minimum_years}")`.  Try to explain this as clearly as possible for 
 # your fellow students.  
 # If you're having trouble, find someone to pair on this explanation with you.
+
+#The sqlite3 gem allows ruby to interface with SQL. In other words, you can create a ruby file, and include SQL code within it (using .execute). The $db variable is holding the database, which contains all three tables (congress_members, voters, and votes). It looks like the execute method works by treating everything typed within the parentheses as SQL code, and returns the resulting table (as an array, i believe). The particular statement $db.execute("SELECT name FROM congress_members WHERE years_in_congress > #{minimum_years}") is returning the table (as an array) with the congress members' names that have been in congress for a longer amount of time than the minimum years variable that was passed in.
